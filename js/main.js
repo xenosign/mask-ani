144f08e47c8e0439c5958c948c8fdb1b2c8b6db0
Tetz <xenosign@naver.com>
2022-08-02 18:23:33 +0900

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+const main = document.querySelector("main");
+const btns = main.querySelectorAll("#nav li");
+const btnsLen = btns.length;
+
+const panels = main.querySelectorAll("section article");
+
+let before = -1;
+
+for (let i = 0; i < btnsLen; i++) {
+    btns[i].addEventListener("click", (e) => {
+        if (before === -1 && i !== 0) {
+            before = i;
+            btns[0].classList.remove("on");
+            btns[i].classList.add("on");
+
+            panels[0].classList.add("mask");
+            panels[i].classList.add("lower");
+
+            setTimeout(() => {
+                panels[0].classList.remove("mask");
+                panels[0].classList.remove("on");
+                panels[i].classList.remove("lower");
+                panels[i].classList.add("on");
+            }, 1400);
+        } else if (before !== i) {
+            btns[before].classList.remove("on");
+            btns[i].classList.add("on");
+
+            panels[before].classList.add("mask");
+            panels[i].classList.add("lower");
+
+            setTimeout(() => {
+                panels[before].classList.remove("mask");
+                panels[before].classList.remove("on");
+                panels[i].classList.remove("lower");
+                panels[i].classList.add("on");
+                before = i;
+            }, 1400);
+
+        }
+    })
+}
+
+
---
+++ js/mask.js
@@ -0,0 +1,5 @@
+const upper = document.querySelector(".upper");
+
+upper.addEventListener("click", e => {
+    e.currentTarget.classList.add("on");
+})
