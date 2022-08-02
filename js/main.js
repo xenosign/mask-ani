b20334f91d38414eb34ecc8efb83da576518553c
kdtTetz <xenosign@naver.com>
2022-08-03 02:53:07 +0900

    불필요 코드 삭제

--- js/main.js
+++ js/main.js
@@ -7,38 +7,35 @@ const panels = main.querySelectorAll("section article");
 let before = -1;
 
 for (let i = 0; i < btnsLen; i++) {
-    btns[i].addEventListener("click", (e) => {
-        if (before === -1 && i !== 0) {
-            before = i;
-            btns[0].classList.remove("on");
-            btns[i].classList.add("on");
+  btns[i].addEventListener("click", (e) => {
+    if (before === -1 && i !== 0) {
+      before = i;
+      btns[0].classList.remove("on");
+      btns[i].classList.add("on");
 
-            panels[0].classList.add("mask");
-            panels[i].classList.add("lower");
+      panels[i].classList.add("lower");
+      panels[0].classList.add("mask");
 
-            setTimeout(() => {
-                panels[0].classList.remove("mask");
-                panels[0].classList.remove("on");
-                panels[i].classList.remove("lower");
-                panels[i].classList.add("on");
-            }, 1400);
-        } else if (before !== i) {
-            btns[before].classList.remove("on");
-            btns[i].classList.add("on");
+      setTimeout(() => {
+        panels[0].classList.remove("mask");
+        panels[0].classList.remove("on");
+        panels[i].classList.remove("lower");
+        panels[i].classList.add("on");
+      }, 1400);
+    } else if (before !== i) {
+      btns[before].classList.remove("on");
+      btns[i].classList.add("on");
 
-            panels[before].classList.add("mask");
-            panels[i].classList.add("lower");
+      panels[i].classList.add("lower");
+      panels[before].classList.add("mask");
 
-            setTimeout(() => {
-                panels[before].classList.remove("mask");
-                panels[before].classList.remove("on");
-                panels[i].classList.remove("lower");
-                panels[i].classList.add("on");
-                before = i;
-            }, 1400);
-
-        }
-    })
+      setTimeout(() => {
+        panels[before].classList.remove("mask");
+        panels[before].classList.remove("on");
+        panels[i].classList.remove("lower");
+        panels[i].classList.add("on");
+        before = i;
+      }, 1400);
+    }
+  });
 }
-
-
